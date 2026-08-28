385a61ea02190c0f8d3ddf7cd220eb3e61de984c
maqa211 <mhmmdqurbanov95@gmail.com>
2026-08-28 12:14:00 +0400

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,19 +1,16 @@
 import json
-import os
 
-def load_data(file_path):
-    if not os.path.exists(file_path):
-        return []
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            content = f.read().strip()
-            if not content:
-                return []
-            return json.loads(content)
-    except Exception:
-        return []
+with open("users.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
 
-def save_data(file_path, data):
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+print(data)
+
+
+with open("books.json", "r",encoding = "utf-8") as file:
+    data_1 = json.load(file)
+
+
+users = {user["id"] : user
+         for user in data_1}
+
+print(users)
